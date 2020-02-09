5446decc12589cd6faaebea0b67e19d7f4563f1b
Stefan Butz <Stefan_Butz@genua.de>
2020-02-09 19:55:03 +0100

    Fix Interrupt Handling

--- asm.h
+++ asm.h
@@ -3,7 +3,8 @@
 
 #include <stdint.h>
 
-extern void halt();
+extern void relax();
+extern void disable_irqs();
 extern uint8_t inb(uint16_t port);
 extern void outb(uint16_t port, uint8_t data);
 
--- idt.c
+++ idt.c
@@ -58,7 +58,7 @@ void idt_init()
 {
 	pic_init();
 
-	idtp.limit = sizeof(struct idt_entry) * 8 - 1;
+	idtp.limit = sizeof(struct idt_entry) * IDT_ENTRIES - 1;
 	idtp.pointer = idt;
 
 	/* 0x8 ist das Kernelcodesegment in der GDT */
@@ -71,73 +71,73 @@ void idt_init()
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
 	idt_set_entry(2, (uintptr_t) &intr_stub_2, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_3, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(3, (uintptr_t) &intr_stub_3, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_4, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(4, (uintptr_t) &intr_stub_4, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_5, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(5, (uintptr_t) &intr_stub_5, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_6, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(6, (uintptr_t) &intr_stub_6, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_7, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(7, (uintptr_t) &intr_stub_7, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_8, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(8, (uintptr_t) &intr_stub_8, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_9, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(9, (uintptr_t) &intr_stub_9, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_10, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(10, (uintptr_t) &intr_stub_10, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_11, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(11, (uintptr_t) &intr_stub_11, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_12, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(12, (uintptr_t) &intr_stub_12, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_13, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(13, (uintptr_t) &intr_stub_13, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_14, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(14, (uintptr_t) &intr_stub_14, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_15, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(15, (uintptr_t) &intr_stub_15, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_16, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(16, (uintptr_t) &intr_stub_16, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_17, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(17, (uintptr_t) &intr_stub_17, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
 
 	/* Hardware-Interrupts (IRQs) */
-	idt_set_entry(2, (uintptr_t) &intr_stub_32, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(32, (uintptr_t) &intr_stub_32, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_33, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(33, (uintptr_t) &intr_stub_33, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_34, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(34, (uintptr_t) &intr_stub_34, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_35, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(35, (uintptr_t) &intr_stub_35, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_36, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(36, (uintptr_t) &intr_stub_36, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_37, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(37, (uintptr_t) &intr_stub_37, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_38, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(38, (uintptr_t) &intr_stub_38, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_39, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(39, (uintptr_t) &intr_stub_39, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_40, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(40, (uintptr_t) &intr_stub_40, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_41, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(41, (uintptr_t) &intr_stub_41, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_42, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(42, (uintptr_t) &intr_stub_42, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_43, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(43, (uintptr_t) &intr_stub_43, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_44, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(44, (uintptr_t) &intr_stub_44, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_45, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(45, (uintptr_t) &intr_stub_45, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_46, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(46, (uintptr_t) &intr_stub_46, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
-	idt_set_entry(2, (uintptr_t) &intr_stub_47, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(47, (uintptr_t) &intr_stub_47, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
 
 	/* Syscalls */
-	idt_set_entry(2, (uintptr_t) &intr_stub_48, 0x8, IDT_FLAG_INTERRUPT_GATE |
+	idt_set_entry(48, (uintptr_t) &intr_stub_48, 0x8, IDT_FLAG_INTERRUPT_GATE |
 		IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_32_BIT);
 
 	idt_load();
@@ -173,13 +173,15 @@ static void pic_init()
 	 * 4. Schicke Flags (0x01).
 	 */
 	outb(0x20, 0x11);
-	outb(0x21, 0x20);
-	outb(0x21, 0x04);
-	outb(0x21, 0x01);
-
 	outb(0xa0, 0x11);
+
+	outb(0x21, 0x20);
 	outb(0xa1, 0x28);
+
+	outb(0x21, 0x04);
 	outb(0xa1, 0x02);
+
+	outb(0x21, 0x01);
 	outb(0xa1, 0x01);
 
 	outb(0x21, 0x0);
@@ -208,8 +210,9 @@ void intr_handler(struct cpu_state cpu)
 	{
 		kprintf("Exception %x, halt Kernel\n", cpu.error_code);
 
+		disable_irqs();
 		while (1)
-			halt();
+			relax();
 	}
 	/* IRQ */
 	else if (cpu.intr < 0x30)
