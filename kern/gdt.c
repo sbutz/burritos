e4aa53a5c2dad6bfe02cde376fdc673564d1e12c
Stefan Butz <stefan260699@gmail.com>
2021-01-10 21:00:58 +0100

    Add struct for tss

--- kern/gdt.c
+++ kern/gdt.c
@@ -2,12 +2,44 @@
 
 #include "gdt.h"
 
+struct tss_entry
+{
+   uint32_t prev_tss;   // Linked list entry if using hardware task switching.
+   uint32_t esp0;       // Stack pointer to load when changing to kernel mode.
+   uint32_t ss0;        // Stack segment to load when changing to kernel mode.
+   uint32_t esp1;       // The remaining fields are unused.
+   uint32_t ss1;
+   uint32_t esp2;
+   uint32_t ss2;
+   uint32_t cr3;
+   uint32_t eip;
+   uint32_t eflags;
+   uint32_t eax;
+   uint32_t ecx;
+   uint32_t edx;
+   uint32_t ebx;
+   uint32_t esp;
+   uint32_t ebp;
+   uint32_t esi;
+   uint32_t edi;
+   uint32_t es;
+   uint32_t cs;
+   uint32_t ss;
+   uint32_t ds;
+   uint32_t fs;
+   uint32_t gs;
+   uint32_t ldt;
+   uint16_t trap;
+   uint16_t iomap_base;
+} __packed;
+typedef struct tss_entry tss_entry_t;
+
 extern void gdt_load();
 extern void tss_load();
 
 static void gdt_set_entry(unsigned int, uint32_t, uint32_t, uint8_t, uint8_t);
 
-static uint32_t tss[26] = {0};
+static tss_entry_t tss;
 
 
 void gdt_init()
@@ -47,14 +79,14 @@ void gdt_init()
 	
 	gdt_load();
 
-	tss[2] = 2 * sizeof(struct gdt_entry); //Index of Kernel Data Segment
+	tss.ss0 = 0x10; // Kernel Data Segment
 	tss_load();
 }
 
 void
-tss_set_kernel_stack(uint32_t esp)
+tss_set_kernel_stack(uint32_t ptr)
 {
-	tss[1] = esp;
+	tss.esp0 = ptr;
 }
 
 
